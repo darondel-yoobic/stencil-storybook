dfe24a0bb9bfa13583316070aa512d8c75cf7056
darondel-yoobic <darondel@yoobic.com>
2023-04-16 15:09:12 +0100

    refactor(storybook): shorten framework value

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from '@storybook/html-webpack5';
 const config: StorybookConfig = {
   addons: ['@storybook/addon-essentials'],
   docs: { autodocs: true },
-  framework: { name: '@storybook/html-webpack5', options: {} },
+  framework: '@storybook/html-webpack5',
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
 };
 
