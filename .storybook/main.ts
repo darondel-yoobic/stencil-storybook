2e65a9fa6ebf9180b8f1f6f33093455ffc4b3c4a
darondel-yoobic <darondel@yoobic.com>
2023-04-16 15:07:29 +0100

    refactor(storybook): switch to TypeScript

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import type { StorybookConfig } from '@storybook/html-webpack5';
+
+const config: StorybookConfig = {
   addons: ['@storybook/addon-essentials'],
   docs: { autodocs: true },
   framework: { name: '@storybook/html-webpack5', options: {} },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
 };
+
+export default config;
